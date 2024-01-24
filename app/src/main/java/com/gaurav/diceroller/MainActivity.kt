8d873eaa8512c504e773624188e9ad151fc8073e
Gaurav <gauravdaultani8@gmail.com>
2024-01-24 20:03:19 +0530

    Initial commit

---
+++ app/src/main/java/com/gaurav/diceroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.gaurav.diceroller
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toDrawable
+
+class MainActivity : AppCompatActivity() {
+    lateinit var RandomButton: Button
+    lateinit var SettoButton: Button
+    lateinit var DiceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        RandomButton = findViewById(R.id.button3)
+        SettoButton=findViewById(R.id.button2)
+        DiceImage=findViewById((R.id.imageView))
+        RandomButton.setOnClickListener{
+            val random=(1..6).random()
+            val drawable=when(random){
+                1->R.drawable.dice_1
+                2->R.drawable.dice_2
+                3->R.drawable.dice_3
+                4->R.drawable.dice_4
+                5->R.drawable.dice_5
+                6->R.drawable.dice_6
+                else->
+                {R.drawable.dice_random}
+            }
+            DiceImage.setImageResource(drawable)
+        }
+
+        SettoButton.setOnClickListener {
+            DiceImage.setImageResource(R.drawable.dice_random)
+        }
+    }
+}
